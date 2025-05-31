a90b5f9af666c8c48dd9ec49f76ce1e96c9cb7a9
marginonlytop <mark.omarbek@gmail.com>
2025-06-01 03:34:56 +0500

    Jwt

---
+++ src/main/java/com/example/taskmanageer/TaskmanageerApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanageer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanageerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanageerApplication.class, args);
+    }
+
+}
